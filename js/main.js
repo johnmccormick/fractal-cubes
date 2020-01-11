434daef96eee7f1326572b2fa286b503ae97572e
John McCormick <johnmccormick91@gmail.com>
2020-01-11 20:22:18 +0000

    moving using delta time

--- js/main.js
+++ js/main.js
@@ -20,10 +20,14 @@ function toRadians(degrees) {
 }
 
 var cameraVelocity = { x: 0, y: 0, z: 0 }
-var cameraRotVelocity = { x: 0, y: 0, z: 0 }
-var cameraAcceleration = { x: 0, y: 0, z: 0 }
+var movementVelocity = 0;
+// var cameraRotVelocity = { x: 0, y: 0, z: 0 }
 
-var cameraAccelerationConstant = 0.02
+var cameraAcceleration = { x: 0, y: 0, z: 0 }
+var movementAcceleration = 0;
+
+var cameraAccelerationConstant = 1
+var movementAccelerationConstant = 0.1
 
 document.body.addEventListener("keydown", e => {
     switch (e.key.toLowerCase()) {
@@ -37,11 +41,11 @@ document.body.addEventListener("keydown", e => {
 
 
         case ('a'):
-            cameraAcceleration.x = -cameraAccelerationConstant;
+            movementAcceleration = -movementAccelerationConstant;
             break;
 
         case ('d'):
-            cameraAcceleration.x = cameraAccelerationConstant;
+            movementAcceleration = movementAccelerationConstant;
             break;
     }
 })
@@ -55,20 +59,20 @@ document.body.addEventListener("keyup", e => {
 
         case ('a'):
         case ('d'):
-            cameraAcceleration.x = 0;
+            movementAcceleration = 0;
             break
     }
 
 })
 
 
-document.body.addEventListener("mousemove", e => {
-    var xVelocity = e.movementX;
-    var yVelocity = e.movementY;
+// document.body.addEventListener("mousemove", e => {
+//     var xVelocity = e.movementX;
+//     var yVelocity = e.movementY;
 
-    cameraRotVelocity.x = yVelocity * -0.001;
-    cameraRotVelocity.y = xVelocity * -0.001;
-})
+//     cameraRotVelocity.x = yVelocity * -0.001;
+//     cameraRotVelocity.y = xVelocity * -0.001;
+// })
 
 var cubeWidth = 1;
 var cube = createCubeMesh(cubeWidth, cubeWidth, cubeWidth, 0x00ff00)
@@ -124,7 +128,7 @@ function someFractalFunction(cubes, cubeWidth, maxDepth, depth) {
     }
 }
 
-function fractalAnimateFunction(cubes, depth) {
+function fractalAnimateFunction(cubes, depth, deltaTime) {
     if (!!cubes) {
         depth = depth + 1;
         var numCubes = cubes.length;
@@ -132,16 +136,16 @@ function fractalAnimateFunction(cubes, depth) {
             var cube = cubes[cubeIndex];
             var vector = new THREE.Vector3(1, 1, 1);
             var normalisedVector = vector.normalize();
-            cube.rotateOnAxis(normalisedVector, 0.01 * depth);
-            fractalAnimateFunction(cube.children, depth);
+            cube.rotateOnAxis(normalisedVector, 1 * depth * deltaTime);
+            fractalAnimateFunction(cube.children, depth, deltaTime);
            
             var parent = cube.parent;
             var vector = new THREE.Vector3( cube.position.x, cube.position.y, cube.position.z )
             vector.normalize();
 
-            cube.position.x += vector.x * cameraVelocity.x
-            cube.position.y += vector.y * cameraVelocity.x;
-            cube.position.z += vector.z * cameraVelocity.x;
+            cube.position.x += vector.x * movementVelocity * deltaTime;
+            cube.position.y += vector.y * movementVelocity * deltaTime;
+            cube.position.z += vector.z * movementVelocity * deltaTime;
         }
     }
 }
@@ -152,7 +156,9 @@ scene.add(cube);
 
 camera.position.z = 6;
 
+var clock = new THREE.Clock()
 function animate() {
+    var deltaTime = clock.getDelta();
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
 
@@ -162,7 +168,6 @@ function animate() {
             `Camera\n` +
             `Position: x = ${camera.position.x.toFixed(2)} | y = ${camera.position.y.toFixed(2)} | z = ${camera.position.z.toFixed(2)}\n` +
             `Velocity: x = ${cameraVelocity.x.toFixed(2)} | y = ${cameraVelocity.y.toFixed(2)} | z = ${cameraVelocity.z.toFixed(2)}\n` +
-            `RotVelocity: x = ${cameraRotVelocity.x.toFixed(2)} | y = ${cameraRotVelocity.y.toFixed(2)} | z = ${cameraRotVelocity.z.toFixed(2)}\n` +
             `\n` +
             `Cube\n` +
             `Rotation: x = ${cube.rotation.x.toFixed(2)} | y = ${cube.rotation.y.toFixed(2)} | z = ${cube.rotation.z.toFixed(2)}`
@@ -174,19 +179,26 @@ function animate() {
     cameraVelocity.y += cameraAcceleration.y;
     cameraVelocity.z += cameraAcceleration.z;
 
-    // camera.position.x += cameraVelocity.x;
-    // camera.position.y += cameraVelocity.y;
-    camera.position.z += cameraVelocity.z;
+    movementVelocity += movementAcceleration;
+
+    camera.position.x += cameraVelocity.x * deltaTime;
+    camera.position.y += cameraVelocity.y * deltaTime;
+    camera.position.z += cameraVelocity.z * deltaTime;
 
     if (cameraVelocity.x != 0) cameraVelocity.x /= 1.15
     if (cameraVelocity.y != 0) cameraVelocity.y /= 1.15
     if (cameraVelocity.z != 0) cameraVelocity.z /= 1.15
 
+    if (movementVelocity != 0) movementVelocity /= 1.15
+
     if (cameraVelocity.x < 0.001 && cameraVelocity.x > -0.001) cameraVelocity.x = 0
     if (cameraVelocity.y < 0.001 && cameraVelocity.y > -0.001) cameraVelocity.y = 0
     if (cameraVelocity.z < 0.001 && cameraVelocity.z > -0.001) cameraVelocity.z = 0
 
-    fractalAnimateFunction(cubes, 0)
+    if (movementVelocity < 0.001 && movementVelocity > -0.001) movementVelocity = 0
+
+    fractalAnimateFunction(cubes, 0, deltaTime)
 
 }
 animate();
+clock.start()
