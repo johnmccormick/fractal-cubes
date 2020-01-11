20ff48bb998b836b606b8f662131e805d50403dd
John McCormick <johnmccormick91@gmail.com>
2020-01-11 18:04:41 +0000

    it works!

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 var scene = new THREE.Scene();
+var cameraDolly = new THREE.Object3D();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+// cameraDolly.add(camera);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -18,6 +20,7 @@ function toRadians(degrees) {
 }
 
 var cameraVelocity = { x: 0, y: 0, z: 0 }
+var cameraRotVelocity = { x: 0, y: 0, z: 0 }
 var cameraAcceleration = { x: 0, y: 0, z: 0 }
 
 var cameraAccelerationConstant = 0.02
@@ -44,7 +47,6 @@ document.body.addEventListener("keydown", e => {
 })
 
 document.body.addEventListener("keyup", e => {
-    console.log(e)
     switch (e.key.toLowerCase()) {
         case ('w'):
         case ('s'):
@@ -57,7 +59,15 @@ document.body.addEventListener("keyup", e => {
             break
     }
 
-    console.log(cameraAcceleration)
+})
+
+
+document.body.addEventListener("mousemove", e => {
+    var xVelocity = e.movementX;
+    var yVelocity = e.movementY;
+
+    cameraRotVelocity.x = yVelocity * -0.001;
+    cameraRotVelocity.y = xVelocity * -0.001;
 })
 
 var cubeWidth = 1;
@@ -76,6 +86,17 @@ cube.add(...subcubes);
 
 camera.position.z = 5;
 
+var vectorArray = [
+    null,
+    new THREE.Vector3( 0, 1, 0),
+    new THREE.Vector3( 1, 0, 0),
+    new THREE.Vector3( 0, 0, 1),
+    new THREE.Vector3( 0, 0, 1),
+    new THREE.Vector3( 1, 0, 0),
+    new THREE.Vector3( 0, 1, 0),
+    null
+]
+
 var index = 0;
 for (var x = -1; x <= 1; x += 2) {
     for (var y = -1; y <= 1; y += 2) {
@@ -84,17 +105,15 @@ for (var x = -1; x <= 1; x += 2) {
             subcubes[index].position.y += (y * ((cubeWidth + subcubeWidth) / 2));
             subcubes[index].position.z += (z * ((cubeWidth + subcubeWidth) / 2));
 
-            // var degrees = toRadians(45);
-            // subcubes[index].rotation.x += (degrees);
-            // subcubes[index].rotation.y += (degrees);
+            var degrees = toRadians(90);
 
+            !!vectorArray[index] && subcubes[index].rotateOnAxis ( vectorArray[index], degrees);
 
             index++
         }
     }
 }
-console.log(subcubes[0].quaternion);
-var rotation = new THREE.Vector3(0, 0, 0);
+
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
@@ -103,10 +122,12 @@ function animate() {
     if (!!info) {
         var infoText = 
         `Camera\n` + 
-        `Position: x = ${cameraVelocity.x} | y = ${cameraVelocity.y} | z = ${cameraVelocity.z}\n` + 
+        `Position: x = ${camera.position.x.toFixed(2)} | y = ${camera.position.y.toFixed(2)} | z = ${camera.position.z.toFixed(2)}\n` + 
+        `Velocity: x = ${cameraVelocity.x.toFixed(2)} | y = ${cameraVelocity.y.toFixed(2)} | z = ${cameraVelocity.z.toFixed(2)}\n` + 
+        `RotVelocity: x = ${cameraRotVelocity.x.toFixed(2)} | y = ${cameraRotVelocity.y.toFixed(2)} | z = ${cameraRotVelocity.z.toFixed(2)}\n` + 
         `\n` +
         `Cube\n` + 
-        `Rotation: x = ${rotation.x} | y = ${rotation.y} | z = ${rotation.z}`
+        `Rotation: x = ${cube.rotation.x.toFixed(2)} | y = ${cube.rotation.y.toFixed(2)} | z = ${cube.rotation.z.toFixed(2)}`
 
         info.innerText = infoText
     }
@@ -119,6 +140,9 @@ function animate() {
     camera.position.y += cameraVelocity.y;
     camera.position.z += cameraVelocity.z;
 
+    // camera.rotation.x += cameraRotVelocity.x;
+    // camera.rotation.y += cameraRotVelocity.y;
+
     if (cameraVelocity.x != 0) cameraVelocity.x /= 1.15
     if (cameraVelocity.y != 0) cameraVelocity.y /= 1.15
     if (cameraVelocity.z != 0) cameraVelocity.z /= 1.15
@@ -127,16 +151,23 @@ function animate() {
     if (cameraVelocity.y < 0.001 && cameraVelocity.y > -0.001) cameraVelocity.y = 0
     if (cameraVelocity.z < 0.001 && cameraVelocity.z > -0.001) cameraVelocity.z = 0
 
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
+    // if (cameraRotVelocity.x != 0) cameraRotVelocity.x /= 1.15
+    // if (cameraRotVelocity.y != 0) cameraRotVelocity.y /= 1.15
+    // if (cameraRotVelocity.z != 0) cameraRotVelocity.z /= 1.15
+
+    // if (cameraRotVelocity.x < 0.001 && cameraRotVelocity.x > -0.001) cameraRotVelocity.x = 0
+    // if (cameraRotVelocity.y < 0.001 && cameraRotVelocity.y > -0.001) cameraRotVelocity.y = 0
+    // if (cameraRotVelocity.z < 0.001 && cameraRotVelocity.z > -0.001) cameraRotVelocity.z = 0
+
+    var vector = new THREE.Vector3( 1, 1, 1);
+    var normalisedVector = vector.normalize();
+    cube.rotateOnAxis ( normalisedVector, 0.01);
     
     var neg = 1;
     for (var index = 0; index < 8; index++) {
-        
-
-        // subcubes[index].rotation.x += (0.02 * neg);
-        // subcubes[index].rotation.y += (0.02 * neg);
-        // subcubes[index].rotation.z += (0.02 * neg);
+        var direction = new THREE.Vector3( 1/3, 1/3, 1/3);
+        var normalisedDirection = direction.normalize();
+        subcubes[index].rotateOnAxis ( normalisedDirection, 0.05 * neg);
         // neg *= -1;
     }
 
